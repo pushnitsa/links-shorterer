39ed5305a9eb9576e4fa18a7346d599e85ed78e2
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-12-13 10:14:25 +0100

    Add links managing controller

---
+++ src/LinksShorterer.Web/Controllers/LinksController.cs
@@ -0,0 +1,26 @@
+﻿using LinksShorterer.Models;
+using LinksShorterer.ShortererService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinksShorterer.Controllers;
+
+[ApiController]
+[Route("api")]
+public class LinksController : ControllerBase
+{
+    private readonly IShorterer _shorterer;
+
+    public LinksController(IShorterer shorterer)
+    {
+        _shorterer = shorterer;
+    }
+
+    [HttpPost]
+    [Route("generate")]
+    public async Task<ActionResult<string>> GenerateShortLink([FromBody] FullLink link)
+    {
+        var result = await _shorterer.GetShortLinkAsync(link.FullUrl);
+
+        return Ok(result);
+    }
+}
---
+++ src/LinksShorterer.Web/Models/FullLink.cs
@@ -0,0 +1,6 @@
+﻿namespace LinksShorterer.Models;
+
+public class FullLink
+{
+    public string FullUrl { get; set; }
+}
