205adc59230cac6de9349a5123992fb0dfa3b8c1
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-12-14 13:30:35 +0100

    Add sample load test

--- LinksShorterer.Tests/UnitTest1.cs
+++ LinksShorterer.Tests/UnitTest1.cs
@@ -1,10 +1,24 @@
+using LinksShorterer.ShortLinkGenerator;
+using System.Diagnostics;
+
 namespace LinksShorterer.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async Task Test1()
     {
+        var shortLinkGenerator = new ShortLinkGeneratorService();
 
+        var timer = new Stopwatch();
+
+        timer.Start();
+        for (var i = 0; i < 1000000000; i++)
+        {
+            await shortLinkGenerator.GenerateShortLinkAsync();
+        }
+        timer.Stop();
+
+        Assert.Equal(0, timer.ElapsedMilliseconds);
     }
 }
