89c22d5b32cc4bbe83217290e31f91b81e656e69
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-12-16 12:03:27 +0100

    Add class to type restrictions

--- src/LinksShorterer.Web/EventManager/IEventListener.cs
+++ src/LinksShorterer.Web/EventManager/IEventListener.cs
@@ -2,7 +2,7 @@
 
 namespace LinksShorterer.EventManager;
 
-public interface IEventListener<TEvent> where TEvent : IEvent
+public interface IEventListener<TEvent> where TEvent : class, IEvent
 {
     Task HandleAsync(TEvent @event);
 }
