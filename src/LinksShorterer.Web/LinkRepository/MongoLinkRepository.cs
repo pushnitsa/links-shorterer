6332c4b57a689d29dd12b5b5fe70c7be45df28cd
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-12-16 11:33:17 +0100

    Add mongodb driver

---
+++ src/LinksShorterer.Web/LinkRepository/MongoLinkRepository.cs
@@ -0,0 +1,26 @@
+﻿using LinksShorterer.Models;
+
+namespace LinksShorterer.LinkRepository;
+
+public class MongoLinkRepository : ILinkRepository
+{
+    public Task CreateLinkAsync(SourceLink sourceLink)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SourceLink?> GetAsync(string shortLinkName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task IncreaseLinkHitsAsync(string shortLinkName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsLinkExistsAsync(string shortLinkName)
+    {
+        throw new NotImplementedException();
+    }
+}
