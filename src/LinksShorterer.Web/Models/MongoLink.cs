592b6d5541b8e292f2e6dfe762aece267b0a8395
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-12-22 13:02:45 +0100

    Remove redundant attributes

--- src/LinksShorterer.Web/Models/MongoLink.cs
+++ src/LinksShorterer.Web/Models/MongoLink.cs
@@ -1,19 +1,12 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace LinksShorterer.Models;
+﻿namespace LinksShorterer.Models;
 
 public class MongoLink
 {
-    [BsonId]
-    public ObjectId Id { get; set; }
+    public Guid Id { get; set; }
 
-    [BsonElement]
     public string ShortLinkName { get; set; }
 
-    [BsonElement]
     public string FullUrl { get; set; }
 
-    [BsonElement]
     public int Hits { get; set; }
 }
