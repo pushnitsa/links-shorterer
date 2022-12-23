e4cf4cbac8bcf91fe8817593e957abaff5373431
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-12-23 17:26:34 +0100

    Add basic bootstrap template

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,30 +1,84 @@
 <template>
-    <nav>
+    <!--<nav>
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
     </nav>
-    <router-view />
+    <router-view /> -->
+    <!-- Header -->
+    <header>
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Fixed navbar</a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <!--<a
+                                class="nav-link active"
+                                aria-current="page"
+                                href="#"
+                            >
+                                Home
+                            </a>-->
+                            <router-link class="nav-link" to="/"
+                                >Home
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/about"
+                                >About
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input
+                            class="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button class="btn btn-outline-success" type="submit">
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="flex-shrink-0">
+        <div class="container">
+            <!-- Page content -->
+            <router-view />
+        </div>
+    </main>
+    <!-- Footer -->
+    <FooterBar />
 </template>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
+<script>
+import FooterBar from "@/components/FooterBar.vue";
 
-nav {
-    padding: 30px;
-}
+export default {
+    components: { FooterBar },
+};
+</script>
 
-nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-    color: #42b983;
+<style scoped>
+main > .container {
+    padding: 60px 15px 0;
 }
 </style>
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
-    </div>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <p class="lead">
+        Pin a footer to the bottom of the viewport in desktop browsers with this
+        custom HTML and CSS. A fixed navbar has been added with
+        <code class="small">padding-top: 60px;</code> on the
+        <code class="small">main &gt; .container</code>.
+    </p>
+    <p>
+        Back to
+        <a href="../examples/sticky-footer/">the default sticky footer</a>
+        minus the navbar.
+    </p>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-    name: "HomeView",
-    components: {
-        HelloWorld,
-    },
-};
-</script>
